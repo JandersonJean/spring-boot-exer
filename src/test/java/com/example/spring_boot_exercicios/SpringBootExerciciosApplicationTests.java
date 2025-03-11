39ec28c173bf54684207a56794c63efcb1a321b1
Janderson Jean <jandersonjean@gmail.com>
2025-03-11 20:56:27 -0300

    First commit

---
+++ src/test/java/com/example/spring_boot_exercicios/SpringBootExerciciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_exercicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExerciciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
