7c660ce05dd9b27684fdf0b51831256869666bb8
Janderson Jean <jandersonjean@gmail.com>
2025-03-12 06:50:37 -0300

    Web Service retornando objeto - JSON

---
+++ src/main/java/com/example/spring_boot_exercicios/controllers/ClienteController.java
@@ -0,0 +1,17 @@
+package com.example.spring_boot_exercicios.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spring_boot_exercicios.models.Cliente;
+
+@RestController
+@RequestMapping(path = "/clientes")
+public class ClienteController {
+	
+	@GetMapping(path = "/qualquer")
+	public Cliente obterCliente() {
+		return new Cliente(30, "Pedro Santos","123.456.789-00");
+	}
+}
