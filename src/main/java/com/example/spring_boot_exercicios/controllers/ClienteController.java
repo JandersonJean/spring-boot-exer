54b54c776d5bc327c6ee641a630938abe5af61d5
Janderson Jean <jandersonjean@gmail.com>
2025-03-13 20:55:04 -0300

    Passando parâmetros para Web Service #01

--- src/main/java/com/example/spring_boot_exercicios/controllers/ClienteController.java
+++ src/main/java/com/example/spring_boot_exercicios/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package com.example.spring_boot_exercicios.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,9 @@ public class ClienteController {
 	@GetMapping(path = "/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(30, "Pedro Santos","123.456.789-00");
+	}
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria Silva","222.333.444-00");
 	}
 }
