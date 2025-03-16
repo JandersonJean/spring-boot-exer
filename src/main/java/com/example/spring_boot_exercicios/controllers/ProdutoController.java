b2a7687337108f455a415f9f342d3468e234269f
Janderson Jean <jandersonjean@gmail.com>
2025-03-16 11:12:11 -0300

    Consulta registro por ID

--- src/main/java/com/example/spring_boot_exercicios/controllers/ProdutoController.java
+++ src/main/java/com/example/spring_boot_exercicios/controllers/ProdutoController.java
@@ -1,7 +1,10 @@
 package com.example.spring_boot_exercicios.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,5 +31,10 @@ public class ProdutoController {
 	@GetMapping
 	public Iterable<Produto> obterProduto(){
 		return produtoRepository.findAll();
+	}
+	
+	@GetMapping(path = "/{id}")
+	public Optional<Produto> obterProdutoId(@PathVariable int id){
+		return produtoRepository.findById(id);
 	}
 }
