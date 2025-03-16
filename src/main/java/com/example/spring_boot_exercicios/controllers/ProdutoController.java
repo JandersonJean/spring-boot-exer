24f0b04514523244ea701a200c63b280c1aa5bce
Janderson Jean <jandersonjean@gmail.com>
2025-03-16 09:50:59 -0300

    Simplificando ao Inserir um novo registro

--- src/main/java/com/example/spring_boot_exercicios/controllers/ProdutoController.java
+++ src/main/java/com/example/spring_boot_exercicios/controllers/ProdutoController.java
@@ -3,7 +3,6 @@ package com.example.spring_boot_exercicios.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +17,8 @@ public class ProdutoController {
 	
 	
 	@PostMapping
-	public @ResponseBody Produto novoProduto(@RequestParam String nome, @RequestParam double preco, @RequestParam double desconto) {
-		Produto produto = new Produto(nome, preco, desconto);
+	public @ResponseBody Produto novoProduto(Produto produto) {
+		//Produto produto = new Produto(nome, preco, desconto);
 		produtoRepository.save(produto);
 		return produto;
 	}
