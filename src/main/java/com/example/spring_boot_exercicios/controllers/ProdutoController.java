774391d00c26f77cb44c45dd0810cccb55846351
Janderson Jean <jandersonjean@gmail.com>
2025-03-16 21:13:05 -0300

    Consulta Paginada

--- src/main/java/com/example/spring_boot_exercicios/controllers/ProdutoController.java
+++ src/main/java/com/example/spring_boot_exercicios/controllers/ProdutoController.java
@@ -3,6 +3,8 @@ package com.example.spring_boot_exercicios.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,6 +40,12 @@ public class ProdutoController {
 	@GetMapping(path = "/{id}")
 	public Optional<Produto> obterProdutoId(@PathVariable int id){
 		return produtoRepository.findById(id);
+	}
+	
+	@GetMapping(path = "/pagina/{numeroPagina}")
+	public Iterable<Produto> obterProdutoPorPagina(@PathVariable int numeroPagina){
+		Pageable page = PageRequest.of(numeroPagina, 2);
+		return produtoRepository.findAll(page);
 	}
 	
 //	@PutMapping
--- src/main/java/com/example/spring_boot_exercicios/model/repositories/ProdutoRepository.java
+++ src/main/java/com/example/spring_boot_exercicios/model/repositories/ProdutoRepository.java
@@ -1,9 +1,10 @@
 package com.example.spring_boot_exercicios.model.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.example.spring_boot_exercicios.model.entities.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer>{
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer>, CrudRepository<Produto, Integer>{
 	
 }
